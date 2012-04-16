236b09835cf163706a8a7ec19a5764be23799029
Logima <logimaru@gmail.com>
2012-04-16 17:52:55 +0300

    poistettu sh-viritykset

--- test-server-start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sh target/bin/webapp &> target/server-output-tests.log &
-echo $! > target/server.pid
--- test-server-stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ -e "target/server.pid" ]; then
-  kill `cat target/server.pid`
-  rm -f target/server.pid
-else
-  echo "no pidfile found"
-fi
